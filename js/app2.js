f06ff95bdc843f62fd7360a36b95e8d410bee787
Ariana Cabana <acabana.0904@gmail.com>
2017-03-27 18:02:35 -0500

    validando tipo de datos e Inputs

--- js/app2.js
+++ js/app2.js
@@ -10,7 +10,7 @@ function Paciente(nombre,apellido,edad,genero,ciudad,pais) {
     var n = this.name + " "+ this.lastname;
     var e = this.age;
     var p = this.country;
-    return "Nombre: " + n + "<br>" + "Edad: " + e + "<br>" + "País: " + p + "<p>";
+    return "<div class='un-paciente'>Nombre: " + n + "<br>" + "Edad: " + e + "<br>" + "País: " + p + "<p></div";
   }
 }
 
@@ -39,3 +39,60 @@ window.addEventListener('load',function(){
     }
   });
 });
+
+// validando inputText
+var nombre = document.getElementById('nombre');
+var apellido = document.getElementById('apellido');
+
+var soloLetras = function(e){
+  var codigoTecla = e.keyCode;
+  console.log(codigoTecla);
+  if ((codigoTecla >= 97 && codigoTecla <= 122) || (codigoTecla >=65 && codigoTecla <= 90) || codigoTecla == 39 || codigoTecla == 32) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+//validando numeros 48 57
+var soloNumeros = function(e){
+  var codeTecla = e.keyCode;
+  console.log(codeTecla);
+  if (codeTecla >= 48 && codeTecla <= 57 && this.value.length < 2) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+//validando datosIngresados
+var inputs = document.getElementsByClassName('input-registro');
+var validaInputs = function(){
+  if(this.value.trim().length==0){
+    this.value="";
+    this.nextElementSibling.nextElementSibling.innerText="*Este campo es obligatorio"
+  }
+  else {
+    this.nextElementSibling.nextElementSibling.innerText="";
+  }
+
+  // var datoCorrecto = this.value.charAt(0).toUpperCase()+this.value.substring(1).toLowerCase();
+  // this.value = datoCorrecto;
+  var arrDato = this.value.split(" ");
+  var datoConMayus = "";
+  for (var i = 0; i < arrDato.length; i++){
+    datoConMayus += arrDato[i].charAt(0).toUpperCase()+arrDato[i].substring(1).toLowerCase()+" ";
+  }
+  this.value = datoConMayus;
+}
+
+for(var i = 0; i < inputs.length; i++){
+  inputs[i].onblur = validaInputs;
+}
+
+edad.onkeypress = soloNumeros;
+nombre.onkeypress = soloLetras;
+apellido.onkeypress = soloLetras;
+pais.onkeypress = soloLetras;
+ciudad.onkeypress = soloLetras;
